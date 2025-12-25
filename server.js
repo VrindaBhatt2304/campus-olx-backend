c88c0c0f696adc57945a4ec9fa7cbab967af9789
Amrit Utsav <lbrock513@gmail.com>
2025-12-25 21:32:37 +0530

    pushed connectDB inside a function

--- server.js
+++ server.js
@@ -16,11 +16,22 @@ app.use(express.urlencoded({extended:true}));
 app.use("/category",category_router);
 app.use("/user",userRoute);
 app.use("/product",productRouter);
-
 const PORT=process.env.PORT || 5000
-connectDB();
-app.listen(PORT, ()=>{
-    console.log("The port is running!");
-})
+
+const startServer = async () => {
+    try {
+        await connectDB();
+        console.log("The database is connected. Yes it is.");
+        app.listen(PORT, () => {
+            console.log("Server is running at ", PORT);
+        })
+    }
+    catch (e) {
+        console.log(e);
+        process.exit(1);
+    }
+}
+
+startServer();
 
 export default app;
